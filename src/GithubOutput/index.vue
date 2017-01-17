3c8d38951132c774e2ab676ffe247e9199c92f2d
Rouzbeh Sarrafieh <rouzbeh.sarrafieh@zooprinting.com>
2017-01-17 13:10:55 -0800

    correct vue version working now

--- src/GithubOutput/index.vue
+++ src/GithubOutput/index.vue
@@ -1,34 +1,3 @@
-import bus from '../bus'
-
-export default {
-  name: 'GithubOutput',
-  created() {
-    bus.$on('new-username', this.onUsernameChange)
-  },
-  destroyed() {
-    bus.$off('new-username', this.onUsernameChange)
-  },
-  methods: {
-    onUsernameChange(name) {
-      this.currentUsername = name
-      this.fetchGithubData(name)
-    },
-    fetchGithubData(name) {
-      // if we have data already, don't request again
-      if (this.githubData.hasOwnProperty(name)) return
-
-      const url = `https://api.github.com/users/${name}`
-      fetch(url)
-        .then(r => r.json())
-        .then(data => {
-          Vue.set(this.githubData, name, data)
-        })
-    },
-  },
-  data() {
-    return {
-      currentUsername: null,
-      githubData: {}
-    }
-  }
-}
+<template src="./template.html"></template>
+<script src="./script.js"></script>
+<style scoped src="./style.css"></style>
